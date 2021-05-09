583dfb8caeec40766c5848f9f46a852d16d2d959
angela <angela.liu1399@gmail.com>
2021-05-09 13:48:35 -0500

    Create patch file

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -6,7 +6,7 @@ import './Graph.css';
 
 interface IProps {
   data: ServerRespond[],
-}
+} 
 
 interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
@@ -23,36 +23,43 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+      price_abc: 'float',       // Used for calculating ratio
+      price_def: 'float',
+      ratio: 'float',           // What we want to track
+      timestamp: 'date',        // Tracking with respect to time
+      upper_bound: 'float',     // Bound for ratio
+      lower_bound: 'float',
+      trigger_alert: 'float',   // Tracks when bounds are crossed
     };
 
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
+
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      // Values we want to see on the y-axis
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([ // GenerateRow no longer returns an array value
         DataManipulator.generateRow(this.props.data),
-      );
+      ]);
     }
   }
 }
